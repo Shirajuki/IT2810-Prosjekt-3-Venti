7ac6b1aa1f43d7610d2d3626dc02bf9f6b6f9a01
heleneelling <helene.elling@outlook.com>
2020-10-30 09:57:03 +0100

    test #9

--- frontend/cypress/integration/search.spec.js
+++ frontend/cypress/integration/search.spec.js
@@ -7,7 +7,8 @@ describe("Test search ", () => {
     
     it("enter data into search field", ()=> {
         cy.get('[data-cy=search-button]').click();
-        cy.get('[data-cy=search]').type('lipstick').type('{enter}');
+        cy.get('[data-cy=search]').type('lipstick').should('have.value', 'lipstick');
+        cy.get('[data-cy=search]').type('{enter}');
         cy.expect('item-display').length.greaterThan(1);
     })
 })
